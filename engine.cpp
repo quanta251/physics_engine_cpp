adc54d33e91f16c55b3b90ae94c83222b3b4fb8c
hayden <haydenbritt25@gmail.com>
2025-08-10 12:49:21 -0500

    started the physicsengine class to hold the simulated world. it has methods for adding particles. particle class now had integrate methods that use implicit euler method to step through time.

--- engine.cpp
+++ engine.cpp
@@ -1,33 +1,22 @@
 #include <cmath>
+#include <cstdlib>
 #include <iostream>
+#include <vector>
 
 struct Vec2 {
     float x, y;
 
     // Constructors
-    Vec2()
-        : x(0.0f), y(0.0f) {}
-
-    Vec2(float xVal, float yVal)
-        : x(xVal), y(yVal) {}
+    Vec2() : x(0.0f), y(0.0f) {}
+    Vec2(float xVal, float yVal) : x(xVal), y(yVal) {}
 
     // Overloads
-    Vec2 operator+(const Vec2& other) const {
-        return Vec2(x + other.x, y + other.y);
-    }
+    Vec2 operator+(const Vec2& other) const { return Vec2(x + other.x, y + other.y); }
+    Vec2 operator*(float scalar) const { return Vec2(x * scalar, y * scalar); }
+    Vec2 operator-(const Vec2& other) const { return Vec2(x - other.x, y - other.y); }
+    Vec2 operator/(float scalar) const { return operator*(1/scalar); }
 
-    Vec2 operator*(float scalar) const {
-        return Vec2(x * scalar, y * scalar);
-    }
-
-    Vec2 operator-(const Vec2& other) const {
-        return Vec2(x - other.x, y - other.y);
-    }
-
-    // This is probably not necessary
-    Vec2 operator/(float scalar) const {
-        return operator*(1/scalar);
-    }
+    Vec2 operator+=(const Vec2& other) { return Vec2( x += other.x, y += other.y); }
 
     // Methods
     float length() {
@@ -35,87 +24,99 @@ struct Vec2 {
         return std::sqrt(length);
     }
 
-    Vec2 unitVec() {
-        return *this/length();
-    }
-
-    float angle() {
-        return std::atan2(y, x);
-    }
-
+    Vec2 unitVec() { return *this/length(); }
+    float angle() { return std::atan2(y, x); }
     float angleDeg() {
-        float angle = std::atan2(y, x);
+        float angle = this->angle();
         return angle * 180 / 3.1415926;
     }
 };
 
 class Particle {
     private:
+        int m_id;
 
     public:
-        Vec2 position, velocity, acceleration;
+        Vec2 position, velocity, forces;
         float mass;
 
-        Particle()
-            : position(Vec2()), velocity(Vec2()), acceleration(Vec2()), mass(0.0f) {}
+        // Constructors
+        Particle() : position(Vec2()), velocity(Vec2()), mass(0.0f), m_id(rand()) {}
+        Particle(Vec2 pos, Vec2 vel, float m, int id) : position(pos), velocity(vel), mass(m), forces(0,0), m_id(id) {}
 
-        // There is no need to construct this with an acceleration because we are going to have a function that calculates this particles acceleration due
-        // to the sum of the forces on this particle
-        Particle(Vec2 pos, Vec2 vel, Vec2 acc, float mass) {
-            position = pos;
-            velocity = vel;
-            acceleration = acc;
-            mass = mass;
+        void addForce(const Vec2& force) {
+            forces += force; // Sum Forces
         }
 
-        void StepThroughTime(float dt) {
-            velocity = velocity + (acceleration * dt);
-            position = position + (velocity * dt);
+        void integrate(float dt) {
+            Vec2 acceleration = forces / mass;
+            velocity += acceleration * dt;
+            position += velocity * dt;
+            clearForces();
+        }
+
+        void clearForces() {
+            forces = Vec2{};
         }
 
         void print() {
-            std::cout << "Position: (" << position.x << ", " << position.y << ") | Velocity: (" << velocity.x << ", " << velocity.y
-                << ") | Acceleration: (" << acceleration.x << ", " << acceleration.y << ")\n";
+            std::cout << "Particle: " << m_id << " | Position: (" << position.x << ", " << position.y << ") | Velocity: (" << velocity.x << ", " << velocity.y << ")\n";
         }
 };
 
+class PhysicsEngine {
+    public:
+        // This will be a vector containing the bodies in the simulation
+        std::vector<Particle> particles;
 
-float calculate_range(float acceleration, float theta, float vo) {
-    return std::sin(2.0f * theta) * vo * vo / acceleration;
-}
+        // Universal Forces/Accelerations here
+        Vec2 gravity = {0.0f, -9.81f}; // m/s^2
 
+        // Add particles to the simulation
+        void addParticle(const Particle& p) {
+            particles.push_back(p);
+        }
+
+        // Step through time
+        void step(float dt) {
+            // 1. Add forces for each particle
+            for (auto& p : particles) {
+                p.addForce(gravity * p.mass);
+            }
+
+            // 2. Integrate all particles
+            for (auto& p : particles) {
+                p.integrate(dt);
+            }
+
+            for (auto& p : particles) {
+                p.print();
+            }
+            std::cout << std::endl;
+
+            accumulateForces();
+            integrateParticles(dt);
+            detectCollisions();
+            resolveCollisions();
+            clearForces();
+        }
+
+    private:
+        void accumulateForces() {}
+        void integrateParticles(float dt) {}
+        void detectCollisions() {}
+        void resolveCollisions() {}
+        void clearForces() {}
+};
 
 int main() {
+    PhysicsEngine engine;
+    engine.addParticle(Particle({0,0}, {5,5}, 2.0f, 1));
+    engine.addParticle(Particle({0,0}, {0,10}, 1.0f, 2) );
 
-    auto position = Vec2(0.0f, 0.0f);
-    auto velocity = Vec2(1.0f, 1.0f);
-    auto acceleration = Vec2(0.0f, -9.81f);
-    float mass = 5.0f; // kg
-
-    Particle p1(position, velocity, acceleration, mass);
-
-    float dt = 0.0001;
-    float max_time = 5.0f;
-    float time = 0;
-
-    for (; time <= max_time; time+=dt) {
-        p1.StepThroughTime(dt);
-
-        std::cout << "Time: " << time << " | ";
-        p1.print();
+    float dt = 1.0f/60.0f;
+    for (int i = 0; i < 300; i++) {
+        std::cout << "Time: " << i * dt << std::endl;
+        engine.step(dt);
     }
-
-    std::cout << "Time: " << time << " | ";
-    p1.print();
-
-    // Calculate the range of this projectile
-    float range = calculate_range(acceleration.length(), velocity.angle(), velocity.length());
-
-    std::cout << "-----------------------------------------------\n";
-    std::cout << "The expected range is: " << range << std::endl;
-
-    std::cout << "-----------------------------------------------\n";
-    std::cout << "The actual range is: " << p1.position.x << std::endl;
-
-    return 0;
 }
